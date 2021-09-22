c7b27bdbe9e1af6f424832ee03ed39434e6b9c6e
James Strong <strong.james.e@gmail.com>
2021-09-22 12:21:57 -0400

    update terraform

--- terraform/app-runner/main.tf
+++ terraform/app-runner/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "" # Enter the Terraform Cloud Organization here
+    organization = "james-strong-contino" # Enter the Terraform Cloud Organization here
     workspaces {
       name = "app-aws-circleci"
     }
